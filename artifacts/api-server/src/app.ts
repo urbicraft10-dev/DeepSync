17c012444217f6e3146175eda61ba9ac4592e413
Rayane <urbicraft10@gmail.com>
2026-07-11 20:44:47 +0100

    deploy 6

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -25,7 +25,12 @@ app.use(
     },
   }),
 );
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  }),
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
